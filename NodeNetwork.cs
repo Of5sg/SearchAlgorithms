31c9cc7576c96ebd09a15976d35cb548af5a816a
Aksel <aksel.kla@gmail.com>
2025-12-04 22:54:59 +0100

    mmade a node network factory

--- NodeNetwork.cs
+++ NodeNetwork.cs
@@ -2,12 +2,12 @@
 
 
 public class Node {
-    private char Value;
-    private Dictionary<string, Node?> Connections;
+    public char Value;
+    public Dictionary<string, Node?> Connections = new Dictionary<string, Node?>();
 
-    public Node(char value, Node Parent, Node child1, Node child2) {
+    public Node(char value, Node? parent, Node? child1, Node? child2) {
         Value = value;
-        Connections["Up"] = Parent;
+        Connections["Up"] = parent;
         Connections["dl"] = child1;
         Connections["dr"] = child2;
     }
@@ -15,16 +15,38 @@ public class Node {
 }
 
 public class TreeFactory {
-
-
-    public Node CreateNodeTree(int LayerDepth, int widthPerLayer, char[] widthElements) {
+    
+    public static Node CreateNodeTree(int LayerDepth, char[] widthElements) {
 
         Node EntryNode = new Node('?', null, null, null);
 
+        List<Node> currentLayerNodes = new List<Node>();
+        List<Node> bottomLayerNodes = new List<Node>();
+        
+        currentLayerNodes.Add(EntryNode);
 
+        int icount = 0;
+        
+        for (int i = 0; i < LayerDepth; i++) {
 
+            foreach (Node node in currentLayerNodes) {
 
+                Node child1 = new Node(widthElements[(icount++ % widthElements.Length)], node, null, null);
+                Node child2 = new Node(widthElements[(icount++ % widthElements.Length)], node, null, null);
 
+                node.Connections["dl"] = child1;
+                node.Connections["dr"] = child2;
+                
+                bottomLayerNodes.Add(child1);
+                bottomLayerNodes.Add(child2);
+
+            }
+
+            currentLayerNodes = bottomLayerNodes;
+
+            bottomLayerNodes = [];
+            
+        }
 
         return EntryNode;
         
--- Program.cs
+++ Program.cs
@@ -6,6 +6,15 @@ namespace SearchAlgorithms {
 
         public static int Main() {
 
+            char[] characters = ['a', 'b', 'c'];
+
+            Node startnode = TreeFactory.CreateNodeTree(2, characters);
+            
+            Console.WriteLine(startnode.Value);
+            
+            Console.WriteLine(startnode.Connections["dl"].Value);
+            Console.WriteLine(startnode.Connections["dr"].Value);
+            
 
             return 0;
         }
